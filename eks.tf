3668d9c6138a3e7e5945b0a4b7d599bea7ebe7e2
Win Thiri Maung <136425988+thirimaung29@users.noreply.github.com>
2024-06-10 11:18:50 +0630

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -24,7 +24,7 @@ module "eks" {
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
-    instance_types = ["m5.large"]
+    instance_types = ["t2.small"]
 
     attach_cluster_primary_security_group = true
   }
