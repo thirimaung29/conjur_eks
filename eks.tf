5e0f88220dccc2b6e8b5e06c1597670386ae9d69
Win Thiri Maung <136425988+thirimaung29@users.noreply.github.com>
2024-06-10 11:11:16 +0630

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,57 +1,42 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "jenkins-vpc"
-  cidr = var.vpc_cidr
-
-  azs = data.aws_availability_zones.azs.names
-
-  private_subnets = var.private_subnets
-  public_subnets  = var.public_subnets
-
-  enable_dns_hostnames = true
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-
-  tags = {
-    "kubernetes.io/cluster/conjur-eks-cluster" = "shared"
-  }
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/conjur-eks-cluster" = "shared"
-    "kubernetes.io/role/elb"               = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/conjur-eks-cluster" = "shared"
-    "kubernetes.io/role/internal-elb"      = 1
-  }
-
-}
-
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.1"
 
-  cluster_name    = "conjur-eks-cluster"
-  cluster_version = "1.24"
-
+  cluster_name                   = local.name
   cluster_endpoint_public_access = true
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  eks_managed_node_groups = {
-    nodes = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 0
-
-      instance_type = ["t2.small"]
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
     }
   }
 
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    instance_types = ["m5.large"]
+
+    attach_cluster_primary_security_group = true
+  }
+
+  eks_managed_node_groups = {
+    amc-cluster-wg = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t2.large"]
+      capacity_type  = "SPOT"
+    }
   }
 }
