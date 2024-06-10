ddee23c7302cfe22ab171bd5c830f1f503c774dd
thirimaung29 <winthiri.maung@brillar.io>
2024-06-10 16:30:44 +0630

    update vm.tf file

--- Jenkins Server/vm.tf
+++ Jenkins Server/vm.tf
@@ -82,7 +82,7 @@ module "ec2_instance" {
   vpc_security_group_ids      = [module.sg.security_group_id]
   subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
-  user_data                   = file("jenkins-install.sh")
+  user_data                   = file("${path.module}/jenkins-install.sh")
   availability_zone           = data.aws_availability_zones.azs.names[0]
 
   tags = {
