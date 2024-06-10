f6e5b9e4eb63babef4b729c2b30476e22e09c57d
Win Thiri Maung <136425988+thirimaung29@users.noreply.github.com>
2024-06-10 11:20:24 +0630

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -32,7 +32,7 @@ module "eks" {
   eks_managed_node_groups = {
     amc-cluster-wg = {
       min_size     = 1
-      max_size     = 2
+      max_size     = 1
       desired_size = 1
 
       instance_types = ["t2.large"]
