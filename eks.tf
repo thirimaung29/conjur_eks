c8e7826482e15984b3b33d18a5f09ef19968cf43
Win Thiri Maung <136425988+thirimaung29@users.noreply.github.com>
2024-06-10 11:20:48 +0630

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -19,7 +19,6 @@ module "eks" {
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.intra_subnets
 
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
