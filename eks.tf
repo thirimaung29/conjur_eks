dde673a3f6d52d710259f75d997f7fcf6cb1ad0a
Win Thiri Maung <136425988+thirimaung29@users.noreply.github.com>
2024-06-10 12:01:16 +0630

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,41 +1,52 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.1"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-  cluster_name                   = local.name
+  name = "jenkins-vpc"
+  cidr = var.vpc_cidr
+
+  azs = data.aws_availability_zones.azs.names
+
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_dns_hostnames = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+
+  tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"               = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"      = 1
+  }
+
+}
+
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.24"
+
   cluster_endpoint_public_access = true
 
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
-
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
-    instance_types = ["t2.small"]
-
-    attach_cluster_primary_security_group = true
-  }
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
-    amc-cluster-wg = {
+    nodes = {
       min_size     = 1
       max_size     = 1
       desired_size = 1
 
-      instance_types = ["t2.large"]
-      capacity_type  = "SPOT"
+      instance_type = ["t2.small"]
     }
   }
 }
