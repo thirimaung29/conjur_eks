2d7d1f5539849a74b94547bce8efbddd4a1936db
Win Thiri Maung <136425988+thirimaung29@users.noreply.github.com>
2024-06-10 14:17:17 +0630

    Update jenkins-install.sh

--- Jenkins Server/jenkins-install.sh
+++ Jenkins Server/jenkins-install.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+#install requirements 
+sudo yum install vim wget jq -y 
+
+# install java 
+sudo yum install java-17-openjdk -y
+
+# install helm
+wget https://get.helm.sh/helm-v3.15.1-linux-amd64.tar.gz
+tar -zxvf helm-v3.15.1-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helm
+
 # install jenkins
 sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade -y
-sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install jenkins -y
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo yum install fontconfig java-17-openjdk -y
+sudo yum install jenkins -y 
+sudo systemctl daemon-reload
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
@@ -16,10 +29,11 @@ sudo yum install git -y
 
 #then install terraform
 sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
 
 #finally install kubectl
-sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
-sudo chmod +x ./kubectl
-sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
