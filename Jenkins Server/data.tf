ebb5b1e54018664b534a321f6ed666857b7c3972
Win Thiri Maung <136425988+thirimaung29@users.noreply.github.com>
2024-06-10 14:16:59 +0630

    Update data.tf

--- Jenkins Server/data.tf
+++ Jenkins Server/data.tf
@@ -1,11 +1,1 @@
-data "aws_ami" "example" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "image-id"
-    values = ["ami-0140c344ea05bbd7a"]
-  }
-}
-
 data "aws_availability_zones" "azs" {}
