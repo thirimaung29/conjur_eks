4cd9d897e59ab21d089e462aff560f8d4bd40583
Win Thiri Maung <136425988+thirimaung29@users.noreply.github.com>
2024-06-10 11:18:19 +0630

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,7 +5,6 @@ locals {
   azs      = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.10.1.0/24", "10.10.2.0/24"]
   private_subnets = ["10.10.3.0/24", "10.10.4.0/24"]
-  intra_subnets   = ["10.10.5.0/24", "10.10.6.0/24"]
 }
 
 provider "aws" {
