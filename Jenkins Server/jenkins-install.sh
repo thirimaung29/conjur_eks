b5e5c658c6762f4041c1d1a5d96b2edbfca07741
thirimaung29 <winthiri.maung@brillar.io>
2024-06-10 16:52:02 +0630

    updating files

--- Jenkins Server/jenkins-install.sh
+++ Jenkins Server/jenkins-install.sh
@@ -11,6 +11,16 @@ wget https://get.helm.sh/helm-v3.15.1-linux-amd64.tar.gz
 tar -zxvf helm-v3.15.1-linux-amd64.tar.gz
 sudo mv linux-amd64/helm /usr/local/bin/helm
 
+# install git
+sudo yum install git -y
+
+#install terraform
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install terraform
+
+
+
 # install jenkins
 sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
@@ -22,19 +32,10 @@ sudo yum install jenkins -y
 sudo systemctl daemon-reload
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
-
-# then install git
-sudo yum install git -y
-
-#then install terraform
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-sudo yum -y install terraform
+sudo systemctl status jenkins
 
 #finally install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
-sudo systemctl status jenkins
